39347653d9256efe3a2c5c05b2da2ba09e7a1537
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-06-05 12:02:35 -0300

    PWA package added

---
+++ lib/pwa/offline_urls.g.dart
@@ -0,0 +1,15 @@
+/// URLs for offline cache.
+final List<String> offlineUrls = [
+  './',
+  './favicon.ico',
+  './main.dart.js',
+  './manifest.json',
+  './packages/browser/dart.js',
+  './packages/browser/interop.js',
+  './packages/front_end/src/fasta/TESTING.md',
+  './packages/front_end/src/fasta/diagnostics.md',
+  './styles.css',
+];
+
+/// Last modified timestamp of the files
+final String lastModified = '2018-06-05T15:00:42.000Z';
