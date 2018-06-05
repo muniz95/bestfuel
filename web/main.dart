d7eeeb9784719be2c5cd70b9cf72e05bfa86c883
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-06-05 17:11:57 -0300

    Service worker and icons added

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,43 @@
 import 'dart:html';
+import 'package:pwa/client.dart' as pwa;
 
 void main() {
-  HtmlElement btnCalculate = document.getElementById('calculate');
-  btnCalculate.onClick.listen((e) {
-    Element txtAlcohol = document.getElementById('alcohol');
-    Element txtGasoline = document.getElementById('gasoline');
+  if (window.location.hostname == 'localhost') {
+    new pwa.Client();
+  } else {
+    new pwa.Client(scriptUrl: '/calculaflex-dart/pwa.dart.js');
+  }
 
-    print(txtAlcohol.text);
-    print(txtGasoline.text);
+  HtmlElement btnCalculate = document.getElementById('calculate');
+  btnCalculate.onClick.listen((Event e) {
+    InputElement txtAlcohol = document.getElementById('alcohol');
+    InputElement txtGasoline = document.getElementById('gasoline');
+
+    double alcohol;
+    if (txtAlcohol.value.contains(',')){
+      alcohol = double.parse(txtAlcohol.value.split(',').join('.'));
+    } else {
+      alcohol = double.parse(txtAlcohol.value);
+    }
+    
+    double gasoline;
+    if (txtGasoline.value.contains(',')){
+      gasoline = double.parse(txtGasoline.value.split(',').join('.'));
+    } else {
+      gasoline = double.parse(txtGasoline.value);
+    }
+
+    calculate(alcohol, gasoline);
   });
+}
+
+void calculate(double alcohol, double gasoline) {
+  double result = alcohol / gasoline;
+  if (result >= 0.6) {
+    document.getElementById('result-gasoline').style.backgroundColor = 'red';
+    document.getElementById('result-alcohol').style.backgroundColor = 'black';
+  } else {
+    document.getElementById('result-alcohol').style.backgroundColor = 'green';
+    document.getElementById('result-gasoline').style.backgroundColor = 'black';
+  }
 }
